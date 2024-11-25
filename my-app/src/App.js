94c5c0fe7d52a5a555d94e8ea4f68f04be016a5c
Ruvindi Chathurya <133088416+MatharaArachchi@users.noreply.github.com>
2024-11-25 14:40:53 +0530

    adding footer

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import { NavBar } from "./components/NavBar";
+import { Footer } from "./components/Footer";
 
 function App() {
   return (
     <div className="App">
       <NavBar />
+      <Footer />
     </div>
   );
 }
